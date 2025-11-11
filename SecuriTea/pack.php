e687cfe219e8a0850edd282d5015c5ce1b0b2334
maigeeen <2401370@s.asojuku.ac.jp>
2025-11-11 12:41:02 +0900

    pack.phpのタグの追加

--- SecuriTea/pack.php
+++ SecuriTea/pack.php
@@ -140,6 +140,7 @@ try {
                                 <label for="triennially">
                                     <span class="plan-name">3年プラン</span>
                                     <span class="plan-price">¥<?php echo number_format($tri_price); ?> / 3年</span>
+                                    <span class="plan-badge">11ヶ月分お得</span>
                                 </label>
                             </div>
                         </fieldset>
