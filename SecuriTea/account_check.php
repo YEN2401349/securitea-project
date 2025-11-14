539d19d2ea30c2661fc080bf88330320819d3eda
Umino611 <2401343@s.asojuku.ac.jp>
2025-11-14 10:51:55 +0900

    カート情報をDBに登録できるように

--- SecuriTea/account_check.php
+++ SecuriTea/account_check.php
@@ -2,7 +2,6 @@
 session_start();
 require 'DBconnect.php'; 
 
-// --- 1. ★★★ 変更点 ★★★ ---
 // (A) カスタムプランがカートにあるか
 $hasCustomPlan = isset($_SESSION['custom_options']) && !empty($_SESSION['custom_options']);
 // (B) パッケージプランがカートにあるか
@@ -10,16 +9,15 @@ $hasPackagePlan = isset($_SESSION['package_plan']);
 
 // (A) も (B) もどちらもカートに無ければ、商品一覧ページに戻す
 if (!$hasCustomPlan && !$hasPackagePlan) {
-    // (戻り先を custom.php から product.php に変更すると、より親切です)
     header('Location: product.php');
     exit;
 }
 
-// --- 2. ログイン状態を確認 (変更なし) ---
+// --- 2. ログイン状態を確認 ---
 if(isset($_SESSION['customer'])){
     // 3a. ログイン済みの場合
-    // どちらかのプランがカートに入っているので cart.php へ
-    header("Location: cart.php");
+    // どちらかのプランがカートに入っているので cart_register.php へ飛ばす
+    header("Location: cart_register.php");
     exit;
 }
 else{
