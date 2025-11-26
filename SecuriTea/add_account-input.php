c61cb25a105d98effa545bf246cf30f7382a089c
kaede-0725 <2401350@s.asojuku.ac.jp>
2025-11-26 12:04:54 +0900

    account-input・link追加

--- SecuriTea/add_account-input.php
+++ SecuriTea/add_account-input.php
@@ -7,6 +7,7 @@
   <title>SecuriTea - アカウント登録</title>
   <link rel="stylesheet" href="css/login-style.css">
   <link rel="stylesheet" href="css/heder-footer.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet"> 
   <style>
     /* プルダウンメニューのスタイルを他の入力欄と合わせるためのサンプル */
