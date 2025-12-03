389fa7c9936d25495ebae59379cb01a3a2be8a09
kaede-0725 <2401350@s.asojuku.ac.jp>
2025-12-03 12:35:39 +0900

    アカウント情報・ログアウトボタン削除

--- SecuriTea/account.php
+++ SecuriTea/account.php
@@ -312,11 +312,6 @@ try {
         </div>
         
       </div>
-      <div class="logout-box">
-        <form action="logout.php" method="post">
-        <button class="btn logout-btn">ログアウト</button>
-        </form>
-      </div>
     </main>
   </div>
   <?php require "./component/modify-pay.php"; ?>
